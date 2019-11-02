162f3fe7a0e8312ad9e23e54b1412cf54e8bd5b6
Elizabeth <elizabethsong65@yahoo.com>
2019-11-02 11:22:57 -0700

    adding search function

---
+++ app/core/questions/questions.module.js
@@ -0,0 +1,1 @@
+angular.module("Questions", []); //defines class
---
+++ app/core/questions/questions.questionsController.js
@@ -0,0 +1,36 @@
+(function () {
+
+    angular.module("Questions").controller("QuestionsController", QuestionsController);
+
+    QuestionsController.$inject = ["$http"];
+
+    function QuestionsController($http) {
+
+        var vm = this;
+        // properties
+        vm.questions = [];
+        vm.isEditing = false;
+
+        // pagination
+        vm.pageSize = 10;
+        vm.currentPage = 0;
+        // vm.numberOfPages = vm.questions.length/vm.pageSize;
+
+        // methods
+        vm.init = init;
+
+
+        function init() {
+            $http.get("http://jservice.io/api/clues")
+                .success(function (data) {
+                    vm.questions = data;
+                })
+        }
+
+        // can call init() directly
+        init();
+
+
+    }
+
+})();
