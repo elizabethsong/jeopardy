b9e575b07e63d82b1d798ed0b8207c3e74a50223
Elizabeth <elizabethsong65@yahoo.com>
2019-11-03 10:23:30 -0800

    final fixes to  search function

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # `angular-seed` — the seed for AngularJS apps
 
 This project is an [AngularJS][angularjs] web app for Jeopardy.
-URL:  https://jeopardyesong.herokuapp.com/
+URL:  <h ref= "https://jeopardyesong.herokuapp.com/">https://jeopardyesong.herokuapp.com/</h>
+Instructions:
+1. Type in URL above. 
+2. First box: type in a category ID value (integer)
+3. Second box: type in a difficulty level: 100,200,300,400,500
+4. Third box: type in a minimum date in date format YYYY-MM-DD **PLEASE NOTE: the Jeopardy API call only works when both a minimum and maximum date is inputted
+5. Fourth box: type in maximum date in date format YYYY-MM-DD
+6. Optional: open up Inspector (F12) for the web page and go to Console to see the Ajax call each time something is typed in
+EXAMPLE call: Category ID 7, Difficulty Level 500, DATES 1985-02-08, 1991-10-30
 
-This was my first time using APIs and learning AngularJS! I had only taken an Intro CS class prior to this but I really enjoyed learning something outside of class and actually 
+This was my first time using APIs and learning AngularJS! I had only taken an Intro CS class prior to this but I really enjoyed learning something outside of class and actually doing a real project that people can access online!
 
 Things I learned/features I implemented:
+- Searching for category ID, difficulty level, dates (calls to API every time a search is typed in). Initially, I only made one call and filtered by these categories
+- How to use APIs/Angular (thanks to many online tutorials and Stack Overflow)
+- Multiple searches so that you can search for category ID AND difficulty level
  
 What I would implement if I had more time:
+- UI/UX Design, specifically
+- Button to make API call to random 
--- app/core/questions/questions.questionsController.js
+++ app/core/questions/questions.questionsController.js
@@ -2,14 +2,30 @@
 
     angular.module("Questions").controller("QuestionsController", QuestionsController);
 
-    QuestionsController.$inject = ["$http"];
+    // $watch based on $scope, so must add the $scope dependency to the controller here if use $watch inside controller
+    QuestionsController.$inject = ["$http", "$scope"];
 
-    function QuestionsController($http) {
+    function QuestionsController($http, $scope) {
 
         var vm = this;
+
+
+        //var apiURL = "http://jservice.io/api/clues?";  // http://jservice.io/  API clues service call URL
+
+        // https://www.mindsumo.com/contests/jeopardy-api
+        //  *API Note: If you are getting CORS issues, some participants have found it to helpful
+        //  to  use the cors-anywhere service.  If you are running into these errors, try using
+        //  the baseURL of https://cors-anywhere.herokuapp.com/http://jservice.io/api/,
+        //  instead of http://jservice.io/api.
+
+        // on Heroku, change api URL to:
+        var apiURL = "https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?";
+
+
         // properties
         vm.questions = [];
         vm.isEditing = false;
+
 
         // pagination
         vm.pageSize = 10;
@@ -18,44 +34,99 @@
             return Math.ceil(vm.questions.length/vm.pageSize);
         };
 
+        // properties for filter search,  used by $scope.$watch()
+        vm.category = 0;
+        vm.difficulty  = 0;
+        vm.startDate = "";
+        vm.endDate = "";
+
         // methods
         vm.init = init;
-        vm.searchC = doSearchforCategory;
-        vm.searchD = doSearchforDifficulty;
-        vm.searchDate = doSearchforDate;
+        // vm.searchC = doSearchforCategory;
+        // vm.searchD = doSearchforDifficulty;
+        // vm.searchDate = doSearchforDate;
 
-
+        // init() method will do initial query to return all questions
         function init() {
-            $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues")
+            $http.get(apiURL)
                 .success(function (data) {
                     vm.questions = data;
                 })
         }
 
-        function doSearchforCategory(category_id) {
+        // use $watch to combine all search parameters on a single $HTTP call search together
+        // combine all 1 parameters:  '[vm.startDate + vm.endDate + vm.category + vm.difficulty]'
+        // Note:  must add vm.xxx  '[startDate + endDate + category + difficulty]'   not working
 
-            $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?category="+category_id)
+        $scope.$watch('[vm.startDate + vm.endDate + vm.category + vm.difficulty]', function () {
+            console.info( 'active filters:  {category_id: ' + vm.category +'} \t {value: ' + vm.difficulty + '} \t  Date: ' +  vm.startDate + '\t' + vm.endDate);
+
+            // Now search on all 4 params
+            searchAll();
+
+            // that was only searching with combine 2 days
+            //searchAll(vm.startDate, vm.endDate);
+        }, true);
+
+        // Search all active filters
+        function searchAll() {
+            // construct API call URL
+            var URL = apiURL;
+
+
+            // !isNaN() make sure the values are number
+            if ( !isNaN(vm.category)  && vm.category > 0)  URL += "category=" + vm.category + "&";
+            if ( !isNaN(vm.difficulty)  && vm.difficulty > 0)  URL += "value=" + vm.difficulty + "&";
+
+            if (isDate(vm.startDate) && isDate(vm.endDate)){
+                URL += "min_date=" + vm.startDate + "&";
+                URL += "max_date=" + vm.endDate + "&";
+            }
+            console.info(URL);
+            // now do $HTTP web service call
+            $http.get(URL)
                 .success(function (data) {
                     vm.questions = data;
                 })
-            vm.currentPage=0
+            vm.currentPage = 0;
         }
-        function doSearchforDifficulty(value) {
 
-            $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?value="+value)
-                .success(function (data) {
-                    vm.questions = data;
-                })
-            vm.currentPage=0
-        }
-        function doSearchforDate(startdate,enddate) {
+        // helper functions
 
-            $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?min_date="+startdate+"&max_date="+enddate)
-                .success(function (data) {
-                    vm.questions = data;
-                })
-            vm.currentPage=0
+        // custom function to check if datestring is a valid Date
+        // somehow angular.isDate()  doesn't work right
+        function isDate(val){
+            var date = Date.parse(val);
+            if (isNaN(date)) return false;
+            else return true;
         }
+
+        //
+        // function doSearchforCategory(category_id) {
+        //
+        //     $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?category="+category_id)
+        //         .success(function (data) {
+        //             vm.questions = data;
+        //         })
+        //     vm.currentPage=0
+        // }
+        // function doSearchforDifficulty(value) {
+        //
+        //     $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?value="+value)
+        //         .success(function (data) {
+        //             vm.questions = data;
+        //         })
+        //     vm.currentPage=0
+        // }
+        // function doSearchforDate(minDate, maxDate) {
+        //
+        //     $http.get("https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues?min_date="+minDate+"&max_date="+maxDate)
+        //         .success(function (data) {
+        //             vm.questions = data;
+        //         })
+        //     vm.currentPage=0
+        // }
+
 
         // can call init() directly
         init();
